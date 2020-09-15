7faae5e1422795e7abeff4d49c0421f4c8720938
Ricart <ricarteusuo11@gmail.com>
2020-09-14 21:44:05 -0300

    feat: adiciona funcionalidade de busca

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
+import api from './services/api'
 
 export default function App() {
-  return (
-    <div>
-      <h1>Fala Tu</h1>
-    </div>
-  );
+    const [search, setSearch] = useState('');
+    const [svg, setSvg] = useState('');
+    const [viewBox, setViewBox] = useState('');
+
+    function handleSearch(e) {
+        e.preventDefault();
+
+        api.get(`/getSvg/${search}`)
+            .then(response => setSvg(response.data[0]['st_assvg']))
+            .catch(err => alert(err));
+
+        api.get(`getViewBox/${search}`)
+            .then(response => setViewBox(response.data[0]['getviewbox']))
+            .catch(err => alert(err));
+
+    }
+    return (
+        <div>
+            <div className='search'>
+                <form onSubmit={handleSearch}>
+                    <input type='text' onChange={event => setSearch(event.target.value)}/>
+                    <input type='submit' value='Enviar'/>
+                </form>
+            </div>
+            <div className='content'>
+                <svg width='200' height='200' viewBox={viewBox}>
+                    <path
+                        d={svg}
+                        stroke='#212121'
+                        strokeWidth='0.001'
+                    />
+                </svg>
+            </div>
+        </div>
+    );
 }
